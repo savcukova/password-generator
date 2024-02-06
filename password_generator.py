4866f89fc313b0aa3beb64326d4e7cf6aa10a33c
savcukova <olinkasavcuk@gmail.com>
2024-02-06 13:03:57 +0100

    variables

---
+++ password_generator.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+
+letters = list(string.ascii_lowercase)
+#returns list of alphabet
+numbers = list(string.digits)
+#return list of numbers in range (10)
+special_char = list(string.punctuation)
+#returns list of !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+
