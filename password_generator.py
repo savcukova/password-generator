bb386262a3ce52cffef4c6e67dbd39e8c52109bc
savcukova <olinkasavcuk@gmail.com>
2024-02-06 13:14:50 +0100

    generate

--- password_generator.py
+++ password_generator.py
@@ -9,3 +9,35 @@ numbers = list(string.digits)
 special_char = list(string.punctuation)
 #returns list of !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
 
+
+def main():
+    inputs()
+
+
+def inputs():
+    letter_count = int(input("How many letters in password? "))
+    number_count = int(input("How many numbers in password? "))
+    char_count = int(input("How many special characters in password? "))
+    
+    generate_password(letter_count, number_count, char_count)
+    
+
+def generate_password(letter_count, number_count, char_count):
+    password = []
+    
+    for x in range(letter_count):
+        password.append(random.choice(letters))
+        
+    for x in range(number_count):
+        password.append(random.choice(numbers))
+        
+    for x in range(char_count):    
+        password.append(random.choice(special_char))
+     
+    joined_password = "".join(password) 
+    print(joined_password)
+
+    
+
+if __name__ == "__main__":
+    main()
